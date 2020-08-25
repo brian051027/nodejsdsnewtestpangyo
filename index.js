88ac1e9593c1042a0e778216d28e7de1132b67d4
brian051027 <seoljunseokor@gmail.com>
2020-08-25 20:31:18 +0900

    heall

--- index.js
+++ index.js
@@ -254,22 +254,6 @@ client.on('message', (message) => {
         })
         .catch(console.error)
     }
-   
-    if(message.content.startsWith("!뮤트")) {
-      if(message.member.hasPermission("MANAGE_MESSAGE")) { // 메세지 관리 권한
-        let user = message.mentions.members.first()
-        let mute = message.guild.roles.cache.find(r => r.id === '747658085383929969').id
-        if(!user) return message.reply("!뮤트 @멘션")
-        user.roles.add(mute).then(member => {
-          message.channel.send(`${member.displayName} 에게 뮤트를 먹였습니다.`)
-        }).catch(() => {
-          message.channel.send(`역할을 지급하지 못했습니다.`)
-        })
-      } else { 
-        message.reply("권한이 없습니다.") 
-      } 
-    }
-  
   } else if(message.content.startsWith('!강퇴')) {
     if(message.channel.type == 'dm') {
       return message.reply('dm에서 사용할 수 없는 명령어 입니다.');
