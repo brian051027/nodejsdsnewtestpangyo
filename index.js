0ebb4059baa56c7832b59ba46e3c207d2fc355d0
brian051027 <seoljunseokor@gmail.com>
2020-08-26 09:39:53 +0900

    last08266

--- index.js
+++ index.js
@@ -193,71 +193,70 @@ client.on('message', (message) => {
         .setFooter(`차재훈 따까리`)
         .setTimestamp()
   
-      embed.addField('공지: ', contents);
+        embed.addField('공지: ', contents);
   
-      message.member.guild.members.array().forEach(x => {
-        if(x.user.bot) return;
-        x.user.send(embed)
-      });
-  
-      return message.reply('공지를 전송했습니다.');
-    } else {
-      return message.reply('채널에서 실행해주세요.');
-    }
-  } else if(message.content.startsWith('!전체공지')) {
-    if(message.member != null) { // 채널에서 공지 쓸 때
-      let contents = message.content.slice('!전체공지'.length);
-      message.member.guild.members.array().forEach(x => {
-        if(x.user.bot) return;
-        x.user.send(`<@${message.author.id}> ${contents}`);
-      });
-  
-      return message.reply('공지를 전송했습니다.');
-    } else {
-      return message.reply('채널에서 실행해주세요.');
-    }
-  } else if(message.content.startsWith('!청소')) {
-    if(message.channel.type == 'dm') {
-      return message.reply('dm에서 사용할 수 없는 명령어 입니다.');
-    }
-  
-      if(message.channel.type != 'dm' && checkPermission(message)) return
-
-    var clearLine = message.content.slice('!청소 '.length);
-    var isNum = !isNaN(clearLine)
-
-    if(isNum && (clearLine <= 0 || 100 < clearLine)) {
-      message.channel.send("1부터 100까지의 숫자만 입력해주세요.")
-      return;
-    } else if(!isNum) { // c @설준서 3
-      if(message.content.split('<@').length == 2) {
-        if(isNaN(message.content.split(' ')[2])) return;
-
-        var user = message.content.split(' ')[1].split('<@!')[1].split('>')[0];
-        var count = parseInt(message.content.split(' ')[2])+1;
-        let _cnt = 0;
-
-        message.channel.fetchMessages().then(collected => {
-          collected.every(msg => {
-            if(msg.author.id == user) {
-              msg.delete();
-              ++_cnt;
-            }
-            return !(_cnt == count);
-          });
+        message.member.guild.members.array().forEach(x => {
+          if(x.user.bot) return;
+          x.user.send(embed)
         });
+    
+        return message.reply('공지를 전송했습니다.');
+      } else {
+        return message.reply('채널에서 실행해주세요.');
       }
-    } else {
-      message.channel.bulkDelete(parseInt(clearLine)+1)
-        .then(() => {
-          AutoMsgDelete(message, `<@${message.author.id}> ` + parseInt(clearLine) + "개의 메시지를 삭제했습니다. (이 메세지는 잠시 후에 사라집니다.)");
-        })
-        .catch(console.error)
-    }
-  } else if(message.content.startsWith('!강퇴')) {
-    if(message.channel.type == 'dm') {
-      return message.reply('dm에서 사용할 수 없는 명령어 입니다.');
-    }
+    } else if(message.content.startsWith('!전체공지')) {
+      if(checkPermission(message)) return
+      if(message.member != null) { // 채널에서 공지 쓸 때
+        let contents = message.content.slice('!전체공지'.length);
+        message.member.guild.members.array().forEach(x => {
+          if(x.user.bot) return;
+          x.user.send(`<@${message.author.id}> ${contents}`);
+        });
+    
+        return message.reply('공지를 전송했습니다.');
+      } else {
+        return message.reply('채널에서 실행해주세요.');
+      }
+    } else if(message.content.startsWith('!청소')) {
+      if(message.channel.type == 'dm') {
+        return message.reply('dm에서 사용할 수 없는 명령어 입니다.');
+      }
+  
+      var clearLine = message.content.slice('!청소 '.length);
+      var isNum = !isNaN(clearLine)
+  
+      if(isNum && (clearLine <= 0 || 100 < clearLine)) {
+        message.channel.send("1부터 100까지의 숫자만 입력해주세요.")
+        return;
+      } else if(!isNum) { // c @나긋해 3
+        if(message.content.split('<@').length == 2) {
+          if(isNaN(message.content.split(' ')[2])) return;
+  
+          var user = message.content.split(' ')[1].split('<@!')[1].split('>')[0];
+          var count = parseInt(message.content.split(' ')[2])+1;
+          let _cnt = 0;
+  
+          message.channel.fetchMessages().then(collected => {
+            collected.every(msg => {
+              if(msg.author.id == user) {
+                msg.delete();
+                ++_cnt;
+              }
+              return !(_cnt == count);
+            });
+          });
+        }
+      } else {
+        message.channel.bulkDelete(parseInt(clearLine)+1)
+          .then(() => {
+            AutoMsgDelete(message, `<@${message.author.id}> ` + parseInt(clearLine) + "개의 메시지를 삭제했습니다. (이 메세지는 잠시 후에 사라집니다.)");
+          })
+          .catch(console.error)
+      }
+    } else if(message.content.startsWith('!강퇴')) {
+      if(message.channel.type == 'dm') {
+        return message.reply('dm에서 사용할 수 없는 명령어 입니다.');
+      }
     
     if(message.channel.type != 'dm' && checkPermission(message)) return
 
